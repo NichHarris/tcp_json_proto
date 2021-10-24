9873be473cb03b71e97796cdcf7d5a95960f022e
Nicholas Kawwas <nicholas.b.kawwas@gmail.com>
2021-10-24 03:47:45 -0400

    Finished JSON Client/Server

--- JSON/client.py
+++ JSON/client.py
@@ -21,84 +21,96 @@ def writeWarningMessage(msg):
 with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
     # Connect to Server Socket
     s.connect((HOSTNAME, PORT))
+    
     print("Client Connected!")
 
-    # Read User Input to Create Request
-    rfw_id = int(input("Enter RFW ID: "))
+    hasRFW = True
+    while hasRFW:
+        # Read User Input to Create Request
+        rfw_id = int(input("\nEnter RFW ID: "))
 
-    # Used to Validate Request
-    isValidated = False
-    while not isValidated:
-        benchmark_type = input("Enter Benchmark Type As 1 (DVD) Or 2 (NDBench): ").strip()
-            
-        # Validate Benchmark Type from Input
-        if benchmark_type == "1":
-            benchmark_type = "DVD"
-            isValidated = not isValidated
-        elif benchmark_type == "2":
-            benchmark_type = "NDBench"
-            isValidated = not isValidated
-        else: 
-            writeWarningMessage("\nInvalid Benchmark Type! Must Enter Either 1 or 2! \n")
+        # Used to Validate Request
+        isValidated = False
+        while not isValidated:
+            benchmark_type = input("Enter Benchmark Type As 1 (DVD) Or 2 (NDBench): ").strip()
+                
+            # Validate Benchmark Type from Input
+            if benchmark_type == "1":
+                benchmark_type = "DVD"
+                isValidated = not isValidated
+            elif benchmark_type == "2":
+                benchmark_type = "NDBench"
+                isValidated = not isValidated
+            else: 
+                writeWarningMessage("\nInvalid Benchmark Type! Must Enter Either 1 or 2! \n")
 
-    
-    isValidated = False
-    while not isValidated:
-        workload_metric = input("Enter Workload Metric As 1 (CPU), 2 (Network In),\n 3 (Network Out) Or 4 (Memory): ").strip()
-
-        # Validate Worklod Metric from Input
-        if workload_metric == "1":
-            workload_metric = "CPUUtilization_Average"
-            isValidated = not isValidated
-        elif workload_metric == "2":
-            workload_metric = "NetworkIn_Average"
-            isValidated = not isValidated
-        elif workload_metric == "3":
-            workload_metric = "NetworkOut_Average"
-            isValidated = not isValidated
-        elif workload_metric == "4":
-            workload_metric = "MemoryUtilization_Average"
-            isValidated = not isValidated
-        else:
-            writeWarningMessage("\nInvalid Workload Metric! Must Enter Either 1, 2, 3, or 4! \n")
-
-    batch_unit = int(input("Enter Batch Unit: "))
-    batch_id = int(input("Enter Batch Id: "))
-    batch_size = int(input("Enter Batch Size: "))
-
-    isValidated = False
-    while not isValidated:
-        data_type = input("Enter Data Type As 1 (Training) Or 2 (Testing): ").strip()
         
-        # Validate Data Type from Input
-        if data_type == "1":
-            data_type = "training"
-            isValidated = not isValidated
-        elif data_type == "2":
-            data_type = "testing"
-            isValidated = not isValidated
-        else: 
-            writeWarningMessage("\nInvalid Data Type! Must Enter Either 1 or 2!\n")
+        isValidated = False
+        while not isValidated:
+            workload_metric = input("Enter Workload Metric As 1 (CPU), 2 (Network In),\n 3 (Network Out) Or 4 (Memory): ").strip()
 
-    # Serialize Request
-    rfw = {"rfw_id": rfw_id,  "benchmark_type": benchmark_type, "workload_metric": workload_metric, "batch_unit": batch_unit, "batch_id": batch_id, "batch_size": batch_size, "data_type": data_type}
+            # Validate Worklod Metric from Input
+            if workload_metric == "1":
+                workload_metric = "CPUUtilization_Average"
+                isValidated = not isValidated
+            elif workload_metric == "2":
+                workload_metric = "NetworkIn_Average"
+                isValidated = not isValidated
+            elif workload_metric == "3":
+                workload_metric = "NetworkOut_Average"
+                isValidated = not isValidated
+            elif workload_metric == "4":
+                workload_metric = "MemoryUtilization_Average"
+                isValidated = not isValidated
+            else:
+                writeWarningMessage("\nInvalid Workload Metric! Must Enter Either 1, 2, 3, or 4! \n")
 
-    # Serialize Request  
-    req = json.dumps(rfw)
+        batch_unit = int(input("Enter Batch Unit: "))
+        batch_id = int(input("Enter Batch Id: "))
+        batch_size = int(input("Enter Batch Size: "))
 
-    # Send Request to Server
-    s.sendall(req.encode("utf-8"))
+        isValidated = False
+        while not isValidated:
+            data_type = input("Enter Data Type As 1 (Training) Or 2 (Testing): ").strip()
+            
+            # Validate Data Type from Input
+            if data_type == "1":
+                data_type = "training"
+                isValidated = not isValidated
+            elif data_type == "2":
+                data_type = "testing"
+                isValidated = not isValidated
+            else: 
+                writeWarningMessage("\nInvalid Data Type! Must Enter Either 1 or 2!\n")
 
-    # Receive Response from Server
-    # 1024 Represents Buffer Size in Bytes
-    data = s.recv(1024)
+        # Serialize Request
+        rfw = {"rfw_id": rfw_id,  "benchmark_type": benchmark_type, "workload_metric": workload_metric, "batch_unit": batch_unit, "batch_id": batch_id, "batch_size": batch_size, "data_type": data_type}
 
-    # Deserialize Response
-    res = data.decode("utf-8")
+        # TODO: Write Request to File
 
-    # Print Response
-    print("\nResponse from Server:")
-    print("Response For Workload Id: ", res['rfw_id'])
-    print("Last Batch Id: ", res['last_batch_id'])
-    print("Requested Data Samples: ", res['data_samples'])
+        # Serialize Request  
+        req = json.dumps(rfw)
 
+        # Send Request to Server
+        s.sendall(req.encode("utf-8"))
+
+        print("\nRequest Sent! Waiting for Response ...\n")
+
+        # Receive Response from Server
+        # 1024 Represents Buffer Size in Bytes
+        data = s.recv(1024)
+
+        # Deserialize Response
+        res = json.loads(data.decode('utf-8'))
+
+        # Print Response
+        print("Response Received!")
+        print(res)
+
+        # TODO: Write Response to File
+
+        # Continue Loop If More Requests Are to Be Done
+        continueRFW = input("\nWant to Request Another Workload (y/n) ? ").strip()
+        if continueRFW[:1] == "n" or continueRFW[:1] == "N":
+            hasRFW = False
+            print("\nClient Socket Closed!\n")
--- JSON/server.py
+++ JSON/server.py
@@ -21,54 +21,75 @@ with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
     # Open TCP Connection
     s.listen()
 
+    print("Server Listening...\n")
+
     # Accept Client Connection
     # - Connection Represents Client Socket Object
     # - Address Represents Client IPv6 Address
     connection, address = s.accept()
 
+    print("Server Connected!\n")
+
     with connection:
         while True:
             # Receive Data from Client Connection
             # 1024 Represents Buffer Size in Bytes
-            data = connection.recv(1024)
+            data = connection.recv(32768)
+
+            # Break and Close Server Socket When Client Socket Closes
+            if not data:
+                print("\nServer Socket Closed!\n")
+                break
+
+            print("Request Received! Perfoming Request ...")
 
             # Deserialize Request
             req = json.loads(data.decode('utf-8'))
+            print(req)
 
             # # Get File to Read
             fileName = f"../data/{req['benchmark_type']}-{req['data_type']}.csv"
-            print(fileName)
 
-            # data_samples = []
+            data_samples = []
 
-            # # Read File Line By Line
-            # with open(fileName, mode = 'r') as file:
-            #     csvLines = csv.reader(file)
+            # Read File Line By Line
+            with open(fileName, mode = 'r') as file:
+                csvReader = csv.reader(file)
 
-            #     # TODO: Validate Batch Unit, Size, and Id Are Valid
+                # Convert to List to Access Rows and Columns
+                csvRows = list(csvReader)
 
-            #     # Number of Batches = Number of Samples / Batch Unit
-            #     numSamples = csvLines.line_num - 1
-            #     numBatches = numSamples/req['batch_unit']
+                # TODO: Validate Batch Unit, Size, and Id Are Valid
 
-            #     startRecord = (req['batch_id']/numBatches) * numSamples 
-            #     endRecord = startRecord + (req['batch_size']/numBatches) * numSamples - 1
+                # Number of Batches = Number of Samples / Batch Unit
+                numSamples = csvReader.line_num - 1
+                numBatches = numSamples/req['batch_unit']
 
-            #     selectedCol = req['workload_metric']
+                startRecord = req['batch_id'] * req['batch_unit']
+                endRecord = startRecord + req['batch_size'] * req['batch_unit'] - 1
+
+                selectedCol = req['workload_metric']
+                if selectedCol == "CPUUtilization_Average":
+                    selectedCol = 1
+                elif selectedCol == "NetworkIn_Average":
+                    selectedCol = 2
+                elif selectedCol == "NetworkOut_Average":
+                    selectedCol = 3
+                elif selectedCol == "MemoryUtilization_Average":
+                    selectedCol = 4
                 
-            #     # Iterate Through CSV, Line By Line
-            #     for i in range(startRecord, endRecord): 
-            #         print(csvLines[i])
+                # , req['batch_unit']
+                for i in range(startRecord, endRecord): 
+                    data_samples.append(csvRows[i][selectedCol - 1])
 
-            # # Not Sure About Last Batch Id
-            # rfd = {"rfw_id": req['rfw_id'], "last_batch_id": req['batch_id'], "data_samples": data_samples} 
 
-            # # Serialize Response
-            # res = rfd.encode('utf-8')
+            last_batch_id = req['batch_id'] + req['batch_size'] - 1
+            rfd = {"rfw_id": req['rfw_id'], "last_batch_id": last_batch_id, "data_samples": data_samples} 
 
-            # # Break Condition
-            # if not res:
-            #     break
+            # Serialize Response
+            res = json.dumps(rfd)
 
-            # # Send All Data Back to Client Socket
-            # connection.sendall(res)
+            # Send All Data Back to Client Socket
+            connection.sendall(res.encode('utf-8'))
+
+            print("\nResponse Sent!\n")
