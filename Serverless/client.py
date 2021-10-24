d0f9776aec2f1cf1454e000b5839173a9dec989a
Nicholas Kawwas <nicholas.b.kawwas@gmail.com>
2021-10-24 05:47:25 -0400

    Added Serverless Version using GRPC. Complete Tomorrow By Inputting and Outputting Real Datapoints

--- Serverless/client.py
+++ Serverless/client.py
@@ -17,11 +17,11 @@ with grpc.insecure_channel("localhost:50051") as c:
     batch_size = 5
     data_type = True
 
-    # TODO: Request Here
-    req = pb.WorkloadRFW(rfw_id, benchmark_type, workload_metric, batch_unit, batch_id, batch_size, data_type)
+    # Request
+    req = pb.WorkloadRFW(rfw_id = rfw_id, benchmark_type = benchmark_type, workload_metric = workload_metric, batch_unit = batch_unit, batch_id = batch_id, batch_size = batch_size, data_type = data_type)
 
-    # TODO: Response Here
-    res = stub.WorkloadRFD(req)
+    # Response
+    res = stub.Workload(req) 
 
     # Print Request and Response Data
-    print(f"RFW: {req}, \nRFD: {res}")
+    print(f"RFW:\n{req} \nRFD:\n{res}")
--- Serverless/server.py
+++ Serverless/server.py
@@ -1,18 +1,36 @@
+import grpc 
+import workload_pb2 as pb
+import workload_pb2_grpc as pb_grpc
+
+# Parallel Threads
+from concurrent import futures 
+
 # Serverless Architecture - Function Based
-class WorkloadServicer(pb_grpc.WorkloadServiceServicer):
+class WorkloadServiceServicer(pb_grpc.WorkloadServiceServicer):
     def Workload(self, request, context):
-        # Request For Workload
+        # Request For Workload Data
         rfw_id = request.rfw_id
         benchmark_type = request.benchmark_type
         workload_metric = request.workload_metric
+        batch_unit = request.batch_unit
         batch_id = request.batch_id
-        batch_size = request.batch_seize
+        batch_size = request.batch_size
         data_type = request.data_type
 
         # TODO: Write Method to Take the Response Arguments to Get RFD Arguments
-    
+        last_batch_id = 2
+        requested_data_samples = [1, 2.0, 3]
+
         # Expected Return Response
-        # return pb.WorkloadRFD(rfw_id, last_batch_id, requested_data_samples)
-        
-        # Since Method to Get RFD Arguments Not in Place, Return Back the RFW for Now
-        return pb.WorkloadRFW(rfw_id, benchmark_type, workload_metric, batch_id, batch_size, data_type)
+        return pb.WorkloadRFD(rfw_id = rfw_id, last_batch_id = last_batch_id, requested_data_samples = requested_data_samples)
+
+# Code From GRPC Python Guide Repo (https://github.com/grpc/grpc/blob/v1.30.0/examples/python/route_guide/route_guide_server.py) 
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pb_grpc.add_WorkloadServiceServicer_to_server( WorkloadServiceServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
